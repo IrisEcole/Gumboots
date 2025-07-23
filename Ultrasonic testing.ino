256cc5e6fbcf76c04a36705136dd7465e56fe0a1
tmckenzie24 <151814613+tmckenzie24@users.noreply.github.com>
2025-07-23 20:35:41 +1000

    Update Ultrasonic testing.ino

--- Ultrasonic testing.ino
+++ Ultrasonic testing.ino
@@ -14,3 +14,32 @@ void loop() {
 
   delay(500);
 */
+//From the internet, but comments are mine :)
+// pins can be any digital pins
+const int trigPin = 9;
+const int echoPin = 10;
+
+float duration, distance;
+
+void setup() {
+  // declare pins
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // creates a pulse from the trigPin for the sensor (echoPin) to read later
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  //pulseIn records the time between a pulse, i.e. from low to high to low
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2; // this converts the duration to distance from sensor
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+}
